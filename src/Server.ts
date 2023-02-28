656c8c2deb793a7bc8e7c3bc9db2e15b834b1c12
Zsolt Gömböcz <zsolt.gombocz00@gmail.com>
2023-02-28 19:51:26 +0100

    inactive user cronjob added

--- src/Server.ts
+++ src/Server.ts
@@ -4,6 +4,9 @@ import { User, UserDataInterface } from './User';
 
 import { v4 as uuidv4 } from 'uuid';
 import { isValidToken } from './utils/isValidToken';
+import { UserStatusEnum } from './enums';
+
+import { CronJob } from 'cron';
 
 export interface ServerInterface {
     server: any,
@@ -18,6 +21,7 @@ export class SocketServer implements ServerInterface {
     server = null;
     queue = Queue.getInstance();
     connectedUsers = [];
+    #inactiveUserCleanup: CronJob;
 
     constructor(http: any) {
         this.server = new Server(http, {
@@ -27,6 +31,39 @@ export class SocketServer implements ServerInterface {
         });
 
         this.server.on('connection', this.onConnect);
+        this.#startInactiveUserCleanupJob();
+    }
+
+    #startInactiveUserCleanupJob = (minutes: number = 5): void => {
+        this.#inactiveUserCleanup = new CronJob(`*/${minutes} * * * *`, async () => {
+            try {
+                this.#deleteDisconnectedUsers(15);
+            } catch (e) {
+                console.error(e);
+            }
+        });
+
+        if (!this.#inactiveUserCleanup.running) {
+            this.#inactiveUserCleanup.start();
+        }
+    }
+
+    //? Filters out the connected users based on passed minutes
+    //? If the user disconnected and on cronjob run he/she/it reached the 'minutes'
+    //? variable (def.: 5 minutes), will be filtered out a.k.a delete the whole User object => no recovering
+    #deleteDisconnectedUsers = (minutes: number = 5): void => {
+        console.log('Inactive user cleanup:');
+        if (this.connectedUsers.length === 0) return console.log('No cleanup need, users = 0');
+
+        const expiredUserList = this.connectedUsers.filter((user: User) => {
+            if (user.getCurrentStatus() !== UserStatusEnum.DISCONNECTED) return false;
+
+            const elapsedTimeInMinutes = Math.floor((Date.now() - user.getTime().leave) / 60000);
+
+            return elapsedTimeInMinutes >= minutes;
+        });
+
+        expiredUserList.forEach((user: User) => this.removeFromConnectedUsers(user.id));
     }
 
     addToConnectedUsers = (user: User): void => {
@@ -54,19 +91,21 @@ export class SocketServer implements ServerInterface {
 
     onConnect = (client): void => {
         const token: string | undefined = this.getAuthToken(client) || uuidv4();
-        let user = new User(token, client, () => this.queue.removeFromQueue(token));
+        let user = new User(token, client);
         this.listenUserInteraction(user, client);
         client.emit('userAuthDone', token);
+        console.log(Date.now());
 
         this.addToConnectedUsers(user);
-
-        this.queue.addToQueue(user, () => {
-            client.emit('addedToQueue', 'nyehehe');
-        });
     }
 
-    listenUserInteraction = (user, client): void => {
+    listenUserInteraction = (user: User, client): void => {
         client.on('updateData', (data: UserDataInterface) => user.updateUserData(data));
+        client.on('startSearch', () => this.queue.addToQueue(user));
         client.on('cancelSearch', () => this.queue.removeFromQueue(user));
+        client.on("disconnect", (reason) => {
+            user.disconnect(Date.now(), reason);
+            this.queue.removeFromQueue(user.getId());
+        });
     }
 }
